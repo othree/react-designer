13271ec9d4a340a1e251c65014bbe70fd0adbaca
othree <othree@gmail.com>
2019-05-24 15:49:05 +0800

    No export default from

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+import Designer from './Designer';
+
 export {Preview} from './Preview';
 export {Vector, Path, Rect, Circle, Text, Image} from './objects';
 export {TextPanel, SizePanel, StylePanel, ArrangePanel, ImagePanel} from './panels';
-export default from './Designer';
+export default Designer;

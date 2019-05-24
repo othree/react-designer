10aa0cbde62714ca1af1cec920dafe4fe750b5e0
othree <othree@gmail.com>
2019-05-24 23:27:56 +0800

    Export Poly

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Designer from './Designer';
 
 export {Preview} from './Preview';
-export {Vector, Path, Rect, Circle, Text, Image} from './objects';
+export {Vector, Path, Rect, Circle, Text, Image, Poly} from './objects';
 export {TextPanel, SizePanel, StylePanel, ArrangePanel, ImagePanel} from './panels';
 export default Designer;

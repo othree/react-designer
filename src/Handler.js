b09c5e30ef41409a3ebc7a90fa7a66ed53b6932c
othree <othree@gmail.com>
2019-06-04 22:48:43 +0800

    Update handler style

--- src/Handler.js
+++ src/Handler.js
@@ -83,22 +83,24 @@ const styles = {
   },
   anchor: {
     'width': 16,
-    'height': 16
+    'height': 16,
+    // 'background': '#fff',
+    'border': '1px solid #fff',
+    // 'box-shadow': '0 0 3px rgba(0, 0, 0, 0.5)'
+    'background': '#03A9F4',
+    'box-shadow': 'none'
   },
   anchorHovered: {
     'background': '#03A9F4',
+    'box-shadow': 'none'
   },
   scaleAnchor: {
-    'background': '#fff',
-    'border': '1px solid #fff',
     'border-radius': 16,
     'position': 'absolute',
     'zIndex': -1
   },
   rotateAnchor: {
     'marginTop': -8,
-    'background': '#fff',
-    'border': '1px solid #fff',
     'border-radius': 16,
     'position': 'absolute',
     'zIndex': -1
